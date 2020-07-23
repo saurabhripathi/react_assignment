c28e750a72af6d5860697268fe1abc38cc1ebae7
saurabh tripathi <saurabh.tripathi@quovantis.com>
2020-07-23 11:27:50 +0530

    assingment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import {createStore} from 'react-redux'
-
-
 
 
 
